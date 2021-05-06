29da4f02b37568e49d6412723931a229386b6295
amoye <amoye@LAPTOP-ET13ETUT>
2021-05-06 11:50:25 -0400

    Add project files.

---
+++ Second.Data/Migrations/202105061340350_add migration attemptTwenty.cs
@@ -0,0 +1,16 @@
+namespace Second.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addmigrationattemptTwenty : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
